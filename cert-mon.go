ac2a2c1fe251de90d8642592ed1ce00faa94e4cc
publicarray <publicarray@users.noreply.github.com>
2023-07-05 23:26:52 +1000

    fix SMTP to SES

--- cert-mon.go
+++ cert-mon.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"net/mail"
 	"net/smtp"
 	"time"
 
@@ -28,8 +29,8 @@ type SMTPConfig struct {
 	Port     int    `toml:"port"`
 	Username string `toml:"username"`
 	Password string `toml:"password"`
-	to       string `toml:"to"`
-	from     string `toml:"from"`
+	To       string `toml:"to"`
+	From     string `toml:"from"`
 }
 
 var configFile string
@@ -55,14 +56,15 @@ func main() {
 	if intervalFlag != 0 {
 		interval = intervalFlag
 	}
-	if interval == 0 {
-		interval = time.Hour
-	}
 	fmt.Println("interval:", interval)
 	fmt.Println("args:", flag.Args())
 
 	// Iterate over the hosts and check certificate validity
 	for _, host := range config.Hosts {
+		// set default port
+		if host.Port == "" {
+			host.Port = "443"
+		}
 		go checkCertificate(host, interval, config.SMTP)
 	}
 
@@ -71,8 +73,10 @@ func main() {
 }
 
 func loadConfig(path string) (Config, error) {
-	var config Config
-
+	//var config Config
+	config := Config{
+		Interval: 60 * 24,
+	}
 	if _, err := toml.DecodeFile(path, &config); err != nil {
 		return config, err
 	}
@@ -134,6 +138,7 @@ func checkCertificate(host HostConfig, checkInterval time.Duration, smtpConfig S
 		// certs := tlsConn.ConnectionState().PeerCertificates
 
 		// Make an HTTP request to the specified hostname and port
+
 		url := fmt.Sprintf("https://%s:%s", host.Hostname, host.Port)
 		req, err := http.NewRequest(http.MethodGet, url, nil)
 		if err != nil {
@@ -181,15 +186,28 @@ func checkCertificate(host HostConfig, checkInterval time.Duration, smtpConfig S
 }
 
 func sendEmailNotification(host HostConfig, daysRemaining int, smtpConfig SMTPConfig) {
+	from := mail.Address{Name: "", Address: smtpConfig.From}
+	to := mail.Address{Name: "", Address: smtpConfig.To}
+	fmt.Printf("from.Address: %s\n", from.Address)
+
 	// Create the email message
 	subject := "Subject: Certificate Expiration Notification\r\n\r\n"
 	// message := fmt.Sprintf("Subject: Certificate Expiration Notification\r\n\r\n")
 	message := fmt.Sprintf("%s The certificate for %s:%s is expiring in %d days.", subject, host.Hostname, host.Port, daysRemaining)
 	// Set up the SMTP client
 	auth := smtp.PlainAuth("", smtpConfig.Username, smtpConfig.Password, smtpConfig.Server)
-
+	tlsconfig := &tls.Config{
+		ServerName: smtpConfig.Server,
+	}
 	// Connect to the SMTP server
-	client, err := smtp.Dial(fmt.Sprintf("%s:%d", smtpConfig.Server, smtpConfig.Port))
+	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", smtpConfig.Server, smtpConfig.Port), tlsconfig)
+	if err != nil {
+		fmt.Printf("TLS Error connecting to the SMTP server: %s\n", err)
+		return
+	}
+	defer conn.Close()
+
+	client, err := smtp.NewClient(conn, smtpConfig.Server)
 	if err != nil {
 		fmt.Printf("Error connecting to the SMTP server: %s\n", err)
 		return
@@ -204,12 +222,12 @@ func sendEmailNotification(host HostConfig, daysRemaining int, smtpConfig SMTPCo
 	}
 
 	// Set the sender and recipient
-	err = client.Mail(smtpConfig.from)
+	err = client.Mail(from.Address)
 	if err != nil {
 		fmt.Printf("Error setting sender: %s\n", err)
 		return
 	}
-	err = client.Rcpt(smtpConfig.to)
+	err = client.Rcpt(to.Address)
 	if err != nil {
 		fmt.Printf("Error setting recipient: %s\n", err)
 		return
